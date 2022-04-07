922ea8f227fae80f0f8c80e7c52b7288a18876e9
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-07 13:56:58 -0300

    Propriedade Componente

--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,20 @@ import Titulo from '../../components/Titulo'
 export default function usandoTitulo() {
   return (
     <div>
-     <Titulo/>
+     <Titulo 
+      principal="Página de cadastro"
+      secundario="Incluir, alterar e excluir"
+     />
+     <Titulo 
+      principal="Página de login"
+      secundario="Informe o seu e-mail e senha"
+      pequeno={true}
+     />
+     <Titulo 
+      principal="Página de login"
+      secundario="Informe o seu e-mail e senha"
+      pequeno
+     />
     </div>
   )
 }
